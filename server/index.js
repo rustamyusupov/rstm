51d4244fab6154809b05d1da1e44ef179dbacd77
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-01-06 16:30:30 +0200

    add dev script

--- server/index.js
+++ server/index.js
@@ -2,15 +2,8 @@ import fastify from 'fastify';
 
 import routes from './routes';
 
-const mode = process.env.NODE_ENV || 'development';
-const isDevelopment = mode === 'development';
-
 export default () => {
-  const app = fastify({
-    logger: {
-      prettyPrint: isDevelopment,
-    },
-  });
+  const app = fastify({});
 
   routes(app);
 
--- server/routes/welcome.js
+++ server/routes/welcome.js
@@ -1,5 +1,6 @@
 export default app => {
   app.get('/', { name: 'root' }, (req, reply) => {
-    reply.render('welcome');
+    // reply.render('welcome');
+    reply.send({ hello: 'world' });
   });
 };
