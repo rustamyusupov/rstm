7b83dfc86c486a4b2f2cea82896855fcc0fcfed5
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-01-07 17:51:57 +0200

    serve html

--- src/server.js
+++ src/server.js
@@ -1,17 +1,27 @@
-const Express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const wishes = require('../public/wishes.json');
 
 const port = process.env.PORT || 8000;
 
 const server = () => {
-  const app = new Express();
+  const app = new express();
 
+  app.use(express.static(path.join(__dirname, '../dist')));
   app.use(bodyParser.json());
 
-  app.get('/wishes', (req, res) => {
+  app.get('/api/wishes', (req, res) => {
     res.send(wishes);
+  });
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
+  });
+
+  app.get('/wishes', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../dist', 'wishes.html'));
   });
 
   return app;
