530d4a120918b176c7c8d0d8383432170ac4ce50
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-01-10 18:59:34 +0200

    update statuses, add settings button

--- frontend/src/pages/wishes/script.js
+++ frontend/src/pages/wishes/script.js
@@ -1,82 +1,13 @@
 import request from '../../utils/request';
 
-const archive = new URL('../../../public/archive.svg', import.meta.url);
-const hidden = new URL('../../../public/hidden.svg', import.meta.url);
+const settings = new URL('../../../public/settings.svg', import.meta.url);
 
-const visibilityMap = {
+const statuses = {
   a: 'archive',
+  b: 'bought',
   h: 'hidden',
-};
-const images = {
-  archive,
-  hidden,
-};
-
-const byCategory =
-  id =>
-  ({ categoryId }) =>
-    categoryId === id;
-const byVisibility =
-  status =>
-  ({ visibility }) =>
-    visibility ? status.includes(visibility) : true;
-
-const getIcon = name =>
-  name
-    ? `<img
-      class="wish-icon"
-      src="${images[name]}"
-      width="14"
-      height="14"
-      alt="visibility"
-      title=${name}
-    />`
-    : '';
-
-const getPrice = (amount, currency) =>
-  Math.ceil(amount).toLocaleString('ru', {
-    style: 'currency',
-    currency,
-    maximumFractionDigits: 0,
-  });
-
-const getWish = ({
-  name,
-  link,
-  price,
-  currency,
-  status,
-  visibility,
-}) => `<li class="wish">
-    ${getIcon(visibility)}
-    <a
-      class="${status}"
-      href="${link}"
-      target="_blank"
-      rel="nofollow noopener"
-    >
-      ${name}
-    </a>
-    &nbsp;&mdash;
-    <span>${getPrice(price, currency)}</span>
-  </li>`;
-
-const render = ({ lists, visibility }) => {
-  const { categories, wishes } = lists;
-  const html = categories
-    .map(({ id, name }) => {
-      const title = `<h4 class="category-title">${name}</h4>`;
-      const list = wishes
-        .filter(byCategory(id))
-        .filter(byVisibility(visibility))
-        .map(getWish)
-        .join('');
-
-      return `<section class="category">${title}<ul>${list}</ul></section>`;
-    })
-    .join('');
-
-  document.querySelector('#root').innerHTML = html;
+  p: 'presented',
+  u: 'unwanted',
 };
 
 const getQuery = search => {
@@ -85,10 +16,79 @@ const getQuery = search => {
   return Object.fromEntries(urlSearchParams.entries());
 };
 
+const byCategory =
+  id =>
+  ({ categoryId }) =>
+    categoryId === id;
+const byVisibility =
+  visibility =>
+  ({ statuses }) =>
+    visibility.length > 0
+      ? visibility.filter(v => statuses.includes(v)).length > 0
+      : statuses.length === 0;
+
+const getPrice = (amount, currency) =>
+  Math.ceil(amount).toLocaleString('ru', {
+    style: 'currency',
+    currency,
+    maximumFractionDigits: 0,
+  });
+
+const getWish =
+  onClick =>
+  ({ name, link, price, currency, statuses }) =>
+    `
+  <li class="wish">
+    <button class="wish-button" type='button' onClick={${onClick}}>
+      <img
+        src="${settings}"
+        width="14"
+        height="14"
+        alt="visibility"
+        title="settings"
+      />
+    </button>
+    <a
+      class="${statuses.join(' ') || 'active'}"
+      href="${link}"
+      target="_blank"
+      rel="nofollow noopener"
+    >
+      ${name}
+    </a>
+    &nbsp;&mdash;
+    <span>${getPrice(price, currency)}</span>
+  </li>
+`;
+
+const render = ({ lists, visibility }) => {
+  const { categories, wishes } = lists;
+  const html = categories
+    .map(({ id, name }) => {
+      const title = `<h4 class="category-title">${name}</h4>`;
+      const handleClick = id => () => console.log(id);
+      const list = wishes
+        .filter(byCategory(id))
+        .filter(byVisibility(visibility))
+        .map(getWish(handleClick(id)))
+        .join('');
+
+      return list !== ''
+        ? `<section class="category">${title}<ul>${list}</ul></section>`
+        : null;
+    })
+    .join('');
+
+  document.querySelector('#root').innerHTML = html;
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
   const lists = await request('/api/db');
+  const { s = '' } = getQuery(window.location.search);
+  const visibility = s
+    .split(',')
+    .filter(String)
+    .map(s => statuses[s]);
 
-  const { v = '' } = getQuery(window.location.search);
-  const visibility = v.split(',').map(k => visibilityMap[k]);
   render({ lists, visibility });
 });
