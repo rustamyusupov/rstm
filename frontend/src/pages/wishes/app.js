427df407e9b515b788348931babb93cafb70a904
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-01-13 12:32:24 +0200

    remove wish active class

--- frontend/src/pages/wishes/app.js
+++ frontend/src/pages/wishes/app.js
@@ -1,28 +1,28 @@
-import cx from 'classnames';
+// import cx from 'classnames';
 
 import request from '../../utils/request';
 
-import * as styles from './styles.module.css';
+// import * as styles from './styles.module.css';
 import { getContent } from './utils';
 import render from './render';
 
-const bindButtonEvents = () => {
-  const buttons = document.querySelectorAll('button[data-wish-id]');
+// const bindButtonEvents = () => {
+//   const buttons = document.querySelectorAll('button[data-wish-id]');
 
-  buttons.forEach(button => {
-    const id = button.dataset.wishId;
-    const item = document.querySelector(`li[data-wish-id='${id}']`);
+//   buttons.forEach(button => {
+//     const id = button.dataset.wishId;
+//     const item = document.querySelector(`li[data-wish-id='${id}']`);
 
-    button.addEventListener('click', () => {
-      item.className = cx(item.className, styles.wish_active);
-    });
-  });
-};
+//     button.addEventListener('click', () => {
+//       item.className = cx(item.className, styles.wish_active);
+//     });
+//   });
+// };
 
 document.addEventListener('DOMContentLoaded', async () => {
   const data = await request('/api/categories');
   const content = getContent(data);
 
   render(content);
-  bindButtonEvents();
+  // bindButtonEvents();
 });
