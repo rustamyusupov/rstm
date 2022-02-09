b27fb643d4a9daa2edd50ac1229579fe1fb0d592
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-02-09 19:39:49 +0200

    render main page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
+const logger = require('morgan');
+
+const index = require('./routes/index');
 
 const port = process.env.PORT || 3000;
-const public = path.join(__dirname, '..', '/public');
+const viewsPath = path.join(__dirname, '/views');
+const publicPath = path.join(__dirname, '..', '/public');
 
 const server = () => {
   const app = new express();
 
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(express.static(public));
+  app.set('view engine', 'pug');
+  app.set('views', viewsPath);
+
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.static(publicPath));
+
+  app.get('/', index);
 
   return app;
 };
---
+++ src/routes/index.js
@@ -0,0 +1,6 @@
+module.exports = (req, res) => {
+  res.render('index', {
+    title: 'Rustam Yusupov',
+    description: 'A little bit about me',
+  });
+};
