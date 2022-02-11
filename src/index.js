29760cde3951297a697637d2eab91c81ef71b7bd
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-02-11 12:46:20 +0200

    add entry point for app

--- src/index.js
+++ src/index.js
@@ -5,27 +5,20 @@ const path = require('path');
 const api = require('./api');
 const routes = require('./routes');
 
-const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', '/public');
 const viewsPath = path.join(__dirname, '/views');
 
-const server = () => {
-  const app = new express();
+const app = new express();
 
-  app.set('view engine', 'pug');
-  app.set('views', viewsPath);
+app.set('view engine', 'pug');
+app.set('views', viewsPath);
 
-  app.use(logger('dev'));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(express.static(publicPath));
-  app.use(routes);
-  app.use(api.routes);
-  app.use(api.router);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(publicPath));
+app.use(routes);
+app.use(api.routes);
+app.use(api.router);
 
-  return app;
-};
-
-server().listen(port, () => {
-  console.log(`Listening on http://localhost:${port}`);
-});
+module.exports = app;
