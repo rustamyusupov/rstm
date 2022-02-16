8bc7d216f808de7dc7a235ca0fc45724ffcf4a4d
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-02-16 09:55:41 +0200

    return api routes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const path = require('path');
 
 const routes = require('./routes');
-// const api = require('./routes/api');
+const api = require('./routes/api');
 
 const publicPath = path.join(__dirname, '..', '/public');
 const viewsPath = path.join(__dirname, '/views');
@@ -18,6 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(publicPath));
 app.use(routes);
-// app.use(api.routes, api.router);
+app.use(api.routes, api.router);
 
 module.exports = app;
