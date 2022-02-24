2f8b4d05f61b550d3306ce746180314e217566a1
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-02-24 13:21:05 +0200

    fix wishes page

--- src/controllers/wishes.js
+++ src/controllers/wishes.js
@@ -2,26 +2,29 @@ const category = require('../models/category');
 const currency = require('../models/currency');
 const wish = require('../models/wish');
 
-const getPrice = ({ price, currency, ...rest }) => ({
-  price: Math.ceil(price).toLocaleString('ru', {
-    style: 'currency',
-    currency,
-    maximumFractionDigits: 0,
-  }),
-  ...rest,
-});
+const getPrice =
+  currencies =>
+  ({ price, currencyId, ...rest }) => ({
+    price: Math.ceil(price).toLocaleString('ru', {
+      style: 'currency',
+      currency: currencies.find(({ id }) => id === currencyId)?.name,
+      maximumFractionDigits: 0,
+    }),
+    ...rest,
+  });
 
-const getCategories = data =>
+const getCategories = (data, currencies) =>
   data
     .filter(category => Array.isArray(category.wishes))
     .map(({ name, wishes }) => ({
       name,
-      wishes: wishes.filter(w => !w.archive).map(getPrice),
+      wishes: wishes.filter(w => !w.archive).map(getPrice(currencies)),
     }));
 
 const list = async (req, res) => {
   const data = await wish.list();
-  const categories = Array.isArray(data) ? getCategories(data) : [];
+  const currencies = await currency.list();
+  const categories = Array.isArray(data) ? getCategories(data, currencies) : [];
 
   res.render('wishes', {
     title: 'Rustam | Wishes',
