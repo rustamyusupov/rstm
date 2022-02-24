89de53d39095c3e9b8ecbaa631db74d56e973847
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-02-24 13:05:06 +0200

    markup wish page

--- src/controllers/wishes.js
+++ src/controllers/wishes.js
@@ -1,13 +1,6 @@
-const wishes = require('../models/wishes');
-
-const statuses = {
-  archive: 'archive',
-  bought: 'bought',
-  hidden: 'hidden',
-  presented: 'presented',
-  unwanted: 'unwanted',
-};
-const hiddenStatuses = [statuses.archive, statuses.hidden];
+const category = require('../models/category');
+const currency = require('../models/currency');
+const wish = require('../models/wish');
 
 const getPrice = ({ price, currency, ...rest }) => ({
   price: Math.ceil(price).toLocaleString('ru', {
@@ -18,19 +11,16 @@ const getPrice = ({ price, currency, ...rest }) => ({
   ...rest,
 });
 
-const notHidden = ({ statuses: s }) =>
-  hiddenStatuses.filter(v => s.includes(v)).length === 0;
-
 const getCategories = data =>
   data
     .filter(category => Array.isArray(category.wishes))
     .map(({ name, wishes }) => ({
       name,
-      wishes: wishes.filter(notHidden).map(getPrice),
+      wishes: wishes.filter(w => !w.archive).map(getPrice),
     }));
 
 const list = async (req, res) => {
-  const data = await wishes.list();
+  const data = await wish.list();
   const categories = Array.isArray(data) ? getCategories(data) : [];
 
   res.render('wishes', {
@@ -41,12 +31,16 @@ const list = async (req, res) => {
 };
 
 const item = async (req, res) => {
-  const data = await wishes.item(req.params.id);
+  const item = await wish.item(req.params.id);
+  const categories = await category.list();
+  const currencies = await currency.list();
 
   res.render('wish', {
     title: 'Rustam | Wish',
     description: 'A little bit of my wish',
-    data,
+    item,
+    categories,
+    currencies,
   });
 };
 
