37593889218e8f76d578a8760dfa44b3403f4f65
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-02-25 18:10:13 +0200

    init visibility button for wishes

---
+++ public/scripts/wishes.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('#visibility');
+
+  button.addEventListener('click', async () => {
+    console.log('visibility');
+  });
+});
