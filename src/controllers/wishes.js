bcc2e8bc91d01c7a2124110a52a7ad19a22b720d
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-08-01 16:32:44 +0200

    update prices for wishes

--- src/controllers/wishes.js
+++ src/controllers/wishes.js
@@ -1,10 +1,48 @@
+const cheerio = require('cheerio');
+
 const category = require('../models/category');
 const currency = require('../models/currency');
 const price = require('../models/price');
 const wish = require('../models/wish');
 
-const formatter = require('../utils/priceFormatter');
-const fetchPrice = require('../utils/fetchPrice');
+const request = require('../utils/request');
+const { decimalDigits, currencies } = require('../utils/constants');
+
+const selectors = {
+  'www.tradeinn.com': '#total_dinamic',
+  'electronics.sony.com': 'div.custom-product-summary__price',
+  'veter.cc': 'div.product-details__product-price.ec-price-item > span',
+};
+
+const updatePrice = async wish => {
+  const { host } = new URL(wish.link);
+  const selector = selectors[host];
+
+  if (!selector) {
+    return wish;
+  }
+
+  const response = await request(wish.link);
+  const $ = cheerio.load(response);
+  const text = $(selector).text();
+  const newCurrency = text.replace(/[\d.,]/g, '').trim();
+  const newPrice = Number(text.replace(/[^\d.,]/g, ''));
+  const isNew =
+    currencies[wish.currency] === newCurrency &&
+    Number(wish.price) !== newPrice;
+
+  return {
+    ...wish,
+    price: isNew ? newPrice : wish.price,
+  };
+};
+
+const formatter = currency =>
+  new Intl.NumberFormat('ru-RU', {
+    style: 'currency',
+    currency: currency,
+    maximumFractionDigits: decimalDigits,
+  });
 
 const categoriseWishes = (categories, wishes) =>
   categories
@@ -22,7 +60,16 @@ const categoriseWishes = (categories, wishes) =>
 const index = async (req, res) => {
   const isAuth = Boolean(req.session?.user?.id);
   const categories = await category.getList();
-  const wishes = await wish.getList(isAuth);
+  const list = await wish.getList(isAuth);
+  const wishes = await Promise.all(list.map(updatePrice));
+  const updated = wishes.filter(
+    ({ id, price }) =>
+      Number(list.find(item => item.id === id).price) !== Number(price)
+  );
+
+  if (isAuth && updated.length !== 0) {
+    await price.addItems(updated);
+  }
 
   res.render('wishes', {
     title: 'Rustam | Wishes',
@@ -30,11 +77,6 @@ const index = async (req, res) => {
     categories: categoriseWishes(categories, wishes),
     editable: isAuth,
   });
-
-  const prices = await Promise.all(wishes.map(fetchPrice));
-  const newPrices = prices.filter(({ price }) => price);
-
-  await price.addItems(newPrices);
 };
 
 const item = async (req, res) => {
