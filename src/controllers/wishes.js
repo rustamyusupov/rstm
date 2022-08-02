e0b41040377dda49b5f8a537d9c5b4149e61de9c
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-08-02 16:00:20 +0200

    remove update wishes prices

--- src/controllers/wishes.js
+++ src/controllers/wishes.js
@@ -1,41 +1,9 @@
-const cheerio = require('cheerio');
-
 const category = require('../models/category');
 const currency = require('../models/currency');
 const price = require('../models/price');
 const wish = require('../models/wish');
 
-const request = require('../utils/request');
-const { decimalDigits, currencies } = require('../utils/constants');
-
-const selectors = {
-  'www.tradeinn.com': '#total_dinamic',
-  'electronics.sony.com': 'div.custom-product-summary__price',
-  'veter.cc': 'div.product-details__product-price.ec-price-item > span',
-};
-
-const updatePrice = async wish => {
-  const { host } = new URL(wish.link);
-  const selector = selectors[host];
-
-  if (!selector) {
-    return wish;
-  }
-
-  const response = await request(wish.link);
-  const $ = cheerio.load(response);
-  const text = $(selector).text();
-  const newCurrency = text.replace(/[\d.,]/g, '').trim();
-  const newPrice = Number(text.replace(/[^\d.,]/g, ''));
-  const isNew =
-    currencies[wish.currency] === newCurrency &&
-    Number(wish.price) !== newPrice;
-
-  return {
-    ...wish,
-    price: isNew ? newPrice : wish.price,
-  };
-};
+const { decimalDigits } = require('../utils/constants');
 
 const formatter = currency =>
   new Intl.NumberFormat('ru-RU', {
@@ -60,16 +28,7 @@ const categoriseWishes = (categories, wishes) =>
 const index = async (req, res) => {
   const isAuth = Boolean(req.session?.user?.id);
   const categories = await category.getList();
-  const list = await wish.getList(isAuth);
-  const wishes = await Promise.all(list.map(updatePrice));
-  const updated = wishes.filter(
-    ({ id, price }) =>
-      Number(list.find(item => item.id === id).price) !== Number(price)
-  );
-
-  if (isAuth && updated.length !== 0) {
-    await price.addItems(updated);
-  }
+  const wishes = await wish.getList(isAuth);
 
   res.render('wishes', {
     title: 'Rustam | Wishes',
