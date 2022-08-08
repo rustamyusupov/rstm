fc3c15f1a6c8607eba07aa685b2f6f4f799635f4
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-08-08 16:52:31 +0200

    fix add wish

--- src/routes/wishes.js
+++ src/routes/wishes.js
@@ -7,6 +7,7 @@ const routes = express.Router();
 
 routes.get('/', wishes.index);
 routes.get('/:id/edit', auth, wishes.item);
+routes.get('/add', auth, wishes.item);
 routes.get('/:id/chart', auth, wishes.chart);
 routes.post('/add', auth, wishes.add);
 routes.post('/:id/edit', auth, wishes.update);
