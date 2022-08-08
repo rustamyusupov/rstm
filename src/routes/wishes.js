dc3b03e894097bb4614b6cfbb0ceb097650228bc
Rustam Yusupov <rustam.yusupov@gmail.com>
2022-08-08 16:24:53 +0200

    separate edit and chart routes for wishes

--- src/routes/wishes.js
+++ src/routes/wishes.js
@@ -6,10 +6,10 @@ const auth = require('../utils/auth');
 const routes = express.Router();
 
 routes.get('/', wishes.index);
-routes.get('/:id', auth, wishes.item);
+routes.get('/:id/edit', auth, wishes.item);
 routes.get('/:id/chart', auth, wishes.chart);
 routes.post('/add', auth, wishes.add);
-routes.post('/:id', auth, wishes.update);
+routes.post('/:id/edit', auth, wishes.update);
 routes.delete('/:id', auth, wishes.remove);
 
 module.exports = routes;
