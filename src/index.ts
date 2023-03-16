db51ce43fc9767d62d8e2db18131be8f3eaf9f77
Rustam Yusupov <rustam.yusupov@gmail.com>
2023-03-16 20:44:14 +0300

    serve app

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
-const sayHello = (subject: string): void => {
-  console.log('Hello ' + subject);
-};
+import express, { Express, Request, Response } from 'express';
+import path from 'path';
 
-sayHello('Test');
+const app: Express = express();
+const port: string = process.env.PORT || '3000';
+const publicPath: string = path.join(__dirname, '..', '/public');
+
+app.use(express.json());
+app.use(express.static(publicPath));
+
+app.use('/', (_: Request, res: Response): void =>
+  res.sendFile(path.join(publicPath, 'index.html'))
+);
+app.use('*', (_: Request, res: Response): void => res.redirect('/'));
+
+app.listen(port, (): void => console.log(`App is running at http://localhost:${port}`));
